70bb0ace73bcfa178fc9a0490034231e0e4f7e9f
edwin onyango <ookoedwin29@gmail.com>
2023-08-18 09:50:07 +0300

    modified frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {Store} from './App/Store';
+import {Store} from "./App/Store";
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
