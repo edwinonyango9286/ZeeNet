9bcf3ef19c9ea2a6ac9c7025adb33ab1b4c22268
edwin onyango <ookoedwin29@gmail.com>
2023-09-05 15:17:51 +0300

    modified adminapp

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {store} from "./app/store"
+import {store} from "./App/Store"
 import { Provider } from 'react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
         <App />
 
 );
+
+
 
