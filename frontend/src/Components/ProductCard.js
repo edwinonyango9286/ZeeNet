eaf36bb2940bbf54e165737d8bb0d164fa2c910a
lloyd  katila <101291544+tonylloyd2@users.noreply.github.com>
2023-09-13 20:36:41 +0300

    home.js complete responsive

--- frontend/src/Components/ProductCard.js
+++ frontend/src/Components/ProductCard.js
@@ -14,58 +14,51 @@ const ProductCard = (props) => {
   let location = useLocation();
   return (
     <>
-      <div
-        className={`${location.pathname == "/store" ? `gr-${grid}` : "col-3"}`}
-      >
-        <Link to={`${location.pathname=="/" ? "/sigleproduct/:id":location.pathname=="/sigleproduct/:id" ? "/sigleproduct/:id":"/sigleproduct/:id"}`}
-         className="product-card position-relative">
-          <div className="wishlist-icon position-absolute">
-            <button className="border-0 bg-transparent">
-              <img src={wishlistIcon} alt="watch"></img>
-            </button>
-          </div>
-          <div className="product-image"> 
-            <img src={watchImage} className="img-fluid" alt="product image" />
-            <img src={watchImage1} className="img-fluid" alt="watch"></img>
-          </div>
+<div className={`col-lg-${grid} col-md-6 col-sm-12`}>
+  <Link
+  to={`${location.pathname=="/" ? "/sigleproduct/:id":location.pathname=="/sigleproduct/:id" ? "/sigleproduct/:id":"/sigleproduct/:id"}`}
+    className="product-card position-relative"
+  >
+    <div className="wishlist-icon position-absolute">
+      <button className="border-0 bg-transparent">
+        <img src={wishlistIcon} alt="watch" />
+      </button>
+    </div>
+    <div className="product-image">
+      <img src={watchImage} className="img-fluid" alt="product image" />
+      <img src={watchImage1} className="img-fluid" alt="watch" />
+    </div>
 
-          <div className="product-details">
-            <h6 className="brand">Vandrose</h6>
-            <h5 className="product-title">
-              Kids headphone bulk 10 pack multi colored for students
-            </h5>
-            <ReactStars
-              count={5}
-              size={20}
-              value={3}
-              edit={false}
-              activeColor="#ffd700"
-            />
-            <p className={`description ${grid === 12 ? "d-bock" : "d-none"}`}>
-              Introducing the HP ProBook X360, a versatile and powerful laptop
-              designed to elevate your productivity and creativity to new
-              heights
-            </p>
-            <p className="price">Ksh 1000/-</p>
-          </div>
+    <div className="product-details">
+      <h6 className="brand">Vandrose</h6>
+      <h5 className="product-title">
+        Kids headphone bulk 10 pack multi-colored for students
+      </h5>
+      <ReactStars count={5} size={20} value={3} edit={false} activeColor="#ffd700" />
+      <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+        Introducing the HP ProBook X360, a versatile and powerful laptop designed to elevate your productivity and creativity to new heights
+      </p>
+      <p className="price">Ksh 1000/-</p>
+    </div>
 
-          <div className="action-bar position-absolute">
-            <div className="d-flex flex-column gap-10">
-              <button className="border-0 bg-transparent">
-                <img src={prodcompare} alt="prodcompare"></img>
-              </button>
+    <div className="action-bar position-absolute">
+      <div className="d-flex flex-column gap-10">
+        <button className="border-0 bg-transparent">
+          <img src={prodcompare} alt="prodcompare" />
+        </button>
 
-              <button className="border-0 bg-transparent">
-                <img src={view} alt="view"></img>
-              </button>
+        <button className="border-0 bg-transparent">
+          <img src={view} alt="view" />
+        </button>
 
-              <button className="border-0 bg-transparent">
-                <img src={addCart} alt="addtocart"></img>
-              </button>
-            </div>
-          </div>
-        </Link>
+        <button className="border-0 bg-transparent">
+          <img src={addCart} alt="addtocart" />
+        </button>
       </div>
+    </div>
+  </Link>
+</div>
+
     </>
   );
 };
--- frontend/src/Components/SpecialProducts.js
+++ frontend/src/Components/SpecialProducts.js
@@ -4,59 +4,63 @@ import watchImage from "../images/watch.jpg";
 import{Link} from "react-router-dom"
 
 
-const SpecialProducts = () => {
+const SpecialProducts = (props) => {
+  
+  
   return (
  
     
-    <div className="col-6 mb-2">
-      <div className="special-product-card">
-        <div className="d-flex justify-content-between">
-          <div>
-            <img src={watchImage} className="img-fluid" alt="watch" />
-          </div>
-          <div className="special-product-content px-2">
-            <h5 className="brand mb-2">Havels</h5>
-            <h6 className="title">Heavenly Horizons: A Celestial Timepiece</h6>
-            <ReactStars
-              count={5}
-              size={20}
-              value={4}
-              edit={false}
-              activeColor="#ffd700"
-            />
-            <p className="price">
-              <span className="red-price">
-                Ksh:1000/-</span>&nbsp; <strike>Ksh:1400/-</strike>
-              
-            </p>
-            <div className="discount-till d-flex align-items-center gap-10">
-              <p className="mb-0">
-                <b>5</b>days
-              </p>
-              <div className="d-flex gap-10">
-                <span className="badge rounded-circle p-2 bg-danger">1</span>:
-                <span className="badge rounded-circle p-2 bg-danger">1</span>:
-                <span className="badge rounded-circle p-2 bg-danger">1</span>
-              </div>
-            </div>
-            <div className="prod-count my-2">
-                <p className="mb-1">Products:5</p>
-                <div class="progress">
-                  <div
-                    className="progress-bar"
-                    role="progressbar"
-                    style={{ width: "25%" }}
-                    aria-valuenow="25"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                  ></div>
-                </div>
-              </div>
-              <Link to="" className="button">Add To Cart</Link>
+<div className={`col-lg-12 col-md-3 col-sm-12`} style={{
+  paddingBottom:"5%",
+}}>
+  <div className="special-product-card">
+    <div className="d-flex justify-content-between">
+      <div className="mb-2">
+        <img src={watchImage} className="img-fluid" alt="watch" />
+      </div>
+      <div className="special-product-content px-2">
+        <h5 className="brand mb-1">Havels</h5>
+        <h6 className="title">Heavenly Horizons: A Celestial Timepiece</h6>
+        <ReactStars
+          count={5}
+          size={20}
+          value={4}
+          edit={false}
+          activeColor="#ffd700"
+        />
+        <p className="price">
+          <span className="red-price">Ksh:1000/-</span>&nbsp;
+          <strike>Ksh:1400/-</strike>
+        </p>
+        <div className="discount-till d-flex align-items-center gap-2">
+          <p className="mb-0">
+            <b>5</b> days
+          </p>
+          <div className="d-flex gap-2">
+            <span className="badge rounded-circle p-2 bg-danger">1</span>:
+            <span className="badge rounded-circle p-2 bg-danger">1</span>:
+            <span className="badge rounded-circle p-2 bg-danger">1</span>
           </div>
         </div>
+        <div className="prod-count my-2">
+          <p className="mb-1">Products: 5</p>
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: "25%" }}
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+        </div>
+        <Link to="" className="button">Add To Cart</Link>
       </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
